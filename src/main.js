5906bf70266f646f810c4919fdbaf3c7ee3d5e13
Mr.Zhan <2695789929@qq.com>
2020-06-11 10:38:25 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { Button } from 'element-ui'
+
+Vue.use(Button)
+
 
 Vue.config.productionTip = false
 
